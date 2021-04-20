d19db98ea04a15610f5ac4a11a0a974a003b4ec7
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-04-20 13:38:38 -0700

    Completed task 3

--- index.js
+++ index.js
@@ -41,11 +41,12 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, getFinalsCB) {
+    const years = getFinalsCB(array).map(item => item.Year);
+    return years;
 }
 
-
+console.log('task 3', getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
@@ -54,7 +55,7 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
+function getWinners(array, getFinalsCB) {
     /* code here */
 }
 
